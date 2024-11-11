716bb5164c8fe2358f5c2955d2531f3963353596
592351665 <592351665@qq.com>
2024-11-11 10:10:55 +0800

    Add a sys_trace() function in kernel/sysproc.c Modify the syscall() function in kernel/syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -145,8 +145,11 @@ syscall(void)
 
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    printf("%s被调用了!\n",syscall_names[num-1]);
+    int trace_mask = p->trace_mask;
     p->trapframe->a0 = syscalls[num]();
+    if((trace_mask>>num) & 1){
+      printf("%d:syscall %s ->%d\n",p->pid,syscall_names[num-1],p->trapframe->a0);
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
