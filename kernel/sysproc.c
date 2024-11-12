3ea0774fdcc2b354bb3199bcd40c1cce902d3cda
592351665 <592351665@qq.com>
2024-11-12 19:20:40 +0800

    尝试

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -112,6 +113,19 @@ sys_trace(void)
 
 uint64
 sys_sysinfo(void){
-  printf("sysinfo say hi!\n");
+  struct sysinfo info;
+  uint64 addr;
+  struct proc *p = myproc();
+
+  info.freemem = -2;
+  info.nproc = -1;
+
+  if(argaddr(0, &addr) < 0)
+    return -1;
+
+  if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+
+
   return 0;
 }
