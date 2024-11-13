9daee51fad84e012ee67e7cde69d77734aeba7a7
592351665 <592351665@qq.com>
2024-11-13 09:31:45 +0800

    fix

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -168,9 +168,7 @@ syscall(void)
 
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    printf("a0前:%d",p->trapframe->a0);
     p->trapframe->a0 = syscalls[num](); 
-    printf("a0后:%d",p->trapframe->a0);
 
     if((1 << num) & p->trace_mask){
       printf("%d:syscall %s ->%d\n",p->pid,syscall_names[num],p->trapframe->a0);
