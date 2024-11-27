ae7bd89aa4a334eea1e2e3b0a9731c5f5ad38423
592351665 <592351665@qq.com>
2024-11-27 10:06:55 +0800

    Lazy allocation

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,20 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if(r_scause() == 13||r_scause() == 15){
+    uint64 va = r_stval();
+    printf("page fault %p\n",va);
+    char *mem =  kalloc();
+    if(mem == 0){
+      p->killed = 1;
+    }
+    memset((void *)mem, 0, PGSIZE);
+    va = PGROUNDDOWN(va);
+    if(mappages(p->pagetable, va, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+      kfree((void *)mem);
+      p->killed = 1;
+    }
+
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
